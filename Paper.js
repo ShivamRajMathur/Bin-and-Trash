3dd461fce40a84fa2b5556b429d4c1051285b0c0
ShivamRajMathur <75162416+ShivamRajMathur@users.noreply.github.com>
2021-01-08 20:17:04 +0000

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,30 @@
+class Paper{
+    constructor(x, y, radius){
+        var options = {
+            'isStatic':false,
+            'restitution':0.3,
+            'friction': 0.5,
+           'density':1.2
+        }
+       this.x = x;
+       this.y = y;
+        this.radius = radius;
+        this.body = Bodies.circle(this.x, this.y, this.radius/2, options); //Why radius/2 impacting ball movement
+               
+        World.add(world, this.body)
+    }
+   
+    display(){
+        var pos =this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rectMode(CENTER);
+        strokeWeight(3)
+        fill("red");
+        ellipse(0,0,this.radius, this.radius);
+        pop();
+              
+    }    
+
+
+}
